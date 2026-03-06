7e00f322a218812583ef130afe01159bc57a5fd4
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2026-03-06 06:07:24 +0100

    spacing

--- src/assignment1.pl
+++ src/assignment1.pl
@@ -1,6 +1,7 @@
 clear :- shell("clear").
 
 % exercise 2-2:
+
 edge(a,b). 
 edge(b,d).
 edge(c,d).
@@ -12,6 +13,7 @@ path(Start,End) :- edge(Start,End).
 path(Start,End) :- edge(Start,Node), path(Node,End).
 
 % exercise 2-3:
+
 ingredient(flour, dough).
 ingredient(egg, dough).
 ingredient(dough, cake).
@@ -25,6 +27,7 @@ partOfReceipe(X, Y) :-
     partOfReceipe(Z, Y).
 
 % exercise 2-4:
+
 reaction(h, h, h2).
 reaction(h2, o, h2o).
 reaction(h, o, oh).
@@ -38,6 +41,7 @@ checkPartOf(X, Y) :-
     checkPartOf(T, Y).
 
 % exercise 2-5:
+
 before(the_hunger_games, the_hunger_games_catching_fire).
 before(the_hunger_games_catching_fire, the_hunger_games_mockingjay_part1).
 before(the_hunger_games_mockingjay_part1, the_hunger_games_mockingjay_part2).
@@ -50,6 +54,7 @@ mustWatchBefore(X, Y) :-
     mustWatchBefore(Z, Y).
 
 % exercise 2-6:
+
 parent(ahmed, karim).
 parent(oumaima, karim).
 parent(souhail, hasna).
@@ -64,6 +69,7 @@ descendant(X, Y) :-
     descendant(X, Z).
 
 % exercise 2-7:
+
 composedOf(aggregate, sand).
 composedOf(aggregate, rock).
 composedOf(aggregate, gravel).
